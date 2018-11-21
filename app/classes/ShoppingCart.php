8197d9953e2b03f3e239bedae1c4d8389d72f5ee
thomas <99040159@mydavinci.nl>
2018-11-21 18:52:32 +0100

    nieuwe code geschreven voor shoppingcart

--- app/classes/ShoppingCart.php
+++ app/classes/ShoppingCart.php
@@ -3,6 +3,7 @@
 namespace App\classes;
 
 use Illuminate\Support\Facades\Session;
+use App\Products;
 
 class ShoppingCart
 {
@@ -10,9 +11,30 @@ class ShoppingCart
     public static function addToSession($id)
     {
 
+        $product = Products::find($id);
+        $Name = $product->Name;
+        $currentCart = session('cart');
+        $Cart = array();
+        if (isset($currentCart)) {
+            if (array_key_exists($Name, $currentCart)) {
+               if ($currentCart[$Name] >= 1){
+                   $newAmount = $currentCart[$Name]+ 1;
+                   $currentCart[$Name] = $newAmount;
+                   session(['cart' => $currentCart]);
+               }
+               else{
+                   return response()->json(['succes' => 'error',]);
+               }
+            } else {
+                $Cart = $currentCart + array ($Name =>1);
+                session(['cart' => $Cart]);
+            }
+        }
+        else{
+            $CartNew = array($Name => 1);
+            session(['cart' => $CartNew]);
+        }
 
-
-        session(['cart' => $Cart]);
 
     }
     public static function GetFromSession(){
